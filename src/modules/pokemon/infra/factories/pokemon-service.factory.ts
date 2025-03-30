6f5fa5365e9c1f850a3696f33a559fb44edbcc01
Joseph Arriaza <jarriaza102@gmail.com>
2025-03-30 09:28:59 +0200

    change way of injecting dependency in pokemon factory

--- src/modules/pokemon/infra/factories/pokemon-service.factory.ts
+++ src/modules/pokemon/infra/factories/pokemon-service.factory.ts
@@ -1,18 +1,9 @@
 import { PokemonService } from '@/modules/pokemon/infra/services/pokemon.service';
-import {
-  createHttpService,
-  pokemonHttpService,
-} from '@/modules/common/infra/factories/http-service.factory';
+import { pokemonHttpService } from '@/modules/common/infra/factories/http-service.factory';
 import { HttpService } from '@/modules/common/infra/services/http.service';
 
-export function createPokemonService(baseUrl?: string) {
-  let httpService = pokemonHttpService;
-
-  if (baseUrl) {
-    createHttpService(baseUrl);
-  }
-
+export function createPokemonService(httpService: HttpService) {
   return new PokemonService(httpService);
 }
 
-export const pokemonService = createPokemonService();
+export const pokemonService = createPokemonService(pokemonHttpService);
